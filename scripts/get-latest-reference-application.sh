d8a41cc95e63aa29af852b1d1436a8ca4acb5557
rosapalli <rosa.palli@thoughtworks.com>
2023-09-14 14:07:32 +0200

    246: fix mistake in get-latest-reference-application.sh

--- scripts/get-latest-reference-application.sh
+++ scripts/get-latest-reference-application.sh
@@ -1,13 +1,14 @@
 TOKEN=$1
 
-#get latest reference application
+#get latest reference application version number
 curl -s https://sourceforge.net/projects/openmrs/files/releases/ >response.html
 grep -o "OpenMRS_Reference_Application_[0-9]*\.[0-9]*\.[0-9]*" response.html >ref_app_versions.txt
 LATEST_REFERENCE_APPLICATION_VERSION=$(head -n 1 ref_app_versions.txt | cut -d '_' -f 4)
 
-#compare with current version and download newer version if available
+#compare with current version number and download newer version files if available
 CURRENT_REFERENCE_APPLICATION_VERSION=$(yq '.project.properties.referenceapplicationVersion' pom.xml)
-if [ "$LATEST_REFERENCE_APPLICATION_VERSION" == "$CURRENT_REFERENCE_APPLICATION_VERSION" ]; then
+
+if [ "$LATEST_REFERENCE_APPLICATION_VERSION" = "$CURRENT_REFERENCE_APPLICATION_VERSION" ]; then
   echo "Already using latest reference application version - $LATEST_REFERENCE_APPLICATION_VERSION"
 else
   echo "Newer reference application version available"
@@ -41,14 +42,15 @@ else
 
   rm sysadmin_data.json sysadmin_response.json
 
-  #replacing modules with newer version available
+  #replacing modules with newer version if there is one available
   ls referenceapplication*/modules >referenceapplication_modules.txt
   echo "Replacing modules with newer version available"
   while IFS=$'\n' read -r latest_module; do
     module_name=$(echo "$latest_module" | cut -d '-' -f 1)
     current_module=$(find docker/web/modules -name "$module_name-*" | cut -d '/' -f 4)
 
-    if [ "$latest_module" == "$current_module" ]; then
+    #compare module version with current version
+    if [ "$latest_module" = "$current_module" ]; then
       echo "Module $module_name is already up to date"
     else
       echo "Committing module version - $latest_module "
@@ -56,6 +58,7 @@ else
       ENCODED_CONTENT=$(base64 -i referenceapplication*/modules/"$latest_module")
       echo '{"message": "updating '"$module_name"' to match reference application version '"$LATEST_REFERENCE_APPLICATION_VERSION'"'", "content":"'"$ENCODED_CONTENT"'"}' >data.json
 
+      #commit new module version
       curl -sL \
         -X PUT \
         -H "Accept: application/vnd.github+json" \
@@ -69,7 +72,9 @@ else
       version=$(echo "$latest_module" | cut -d '-' -f 2 | cut -d '.' -f 1-3)
       yq -i '.project.properties.'"$module_name"'Version = "'"$version"'"' pom.xml
 
+      #delete outdated module version from repo
       echo "Deleting outdated module - $current_module"
+      #get file sha
       curl -sL \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer $TOKEN" \
@@ -78,6 +83,7 @@ else
 
       SHA=$(jq -r '.sha' delete_response.json)
 
+      #delete file
       curl -sL \
         -X DELETE \
         -H "Accept: application/vnd.github+json" \
@@ -86,7 +92,8 @@ else
         https://api.github.com/repos/BLOPUP-UPC/blopup-openmrs-distribution/contents/docker/web/modules/"$current_module" \
         -d '{"message":"removing outdated module '"$current_module"'", "sha":"'"$SHA"'"}' >delete_response.json
     fi
-  done <referenceapplication_modules.txt
+  done < referenceapplication_modules.txt
+
   if grep -q "modules" response.json; then
     echo "Updating reference application version to $LATEST_REFERENCE_APPLICATION_VERSION"
   else
