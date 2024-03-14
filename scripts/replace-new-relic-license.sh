66ada7894659f9341b03f96a8b6f53db2bc5340b
Alejandro Batanero <alejandro.batanero@thoughtworks.com>
2024-03-14 16:27:36 +0100

    BLOPUP-401 Fix sed command

--- scripts/replace-new-relic-license.sh
+++ scripts/replace-new-relic-license.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-. scripts/functions.sh
 NEW_RELIC_LICENSE_KEY=$1
 
-sed -i '' "s/{{NEW_RELIC_LICENSE_KEY}}/$NEW_RELIC_LICENSE_KEY/g" docker/web/newrelic/newrelic.yml
-sed -i '' "s/{{NEW_RELIC_LICENSE_KEY}}/$NEW_RELIC_LICENSE_KEY/g" docker/web/newrelic/newrelic-infra.yml
+sed -i'' -e "s/{{NEW_RELIC_LICENSE_KEY}}/$NEW_RELIC_LICENSE_KEY/g" docker/web/newrelic/newrelic.yml
+sed -i'' -e "s/{{NEW_RELIC_LICENSE_KEY}}/$NEW_RELIC_LICENSE_KEY/g" docker/web/newrelic/newrelic-infra.yml
